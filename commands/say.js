a03c5db496914dfda83157754c04fa5ee58a1880
Anirudh242 <anirudhbhardwaj48@gmail.com>
2020-12-26 21:50:33 +0530

    Added @everyone filter

--- commands/say.js
+++ commands/say.js
@@ -4,17 +4,30 @@ module.exports = {
 	name: "say",
 	description: " ",
 	execute(message, args, embedFooter) {
-		filter = new Filter();
-		let removeWords = ["hells", "hell", "shit", "shits", "sadist"];
+		// New filter
+		const filter = new Filter();
 
+		// Message user wants the bot to say
+		let user = message.author;
+		let botMessage = args.join(" ");
+
+		// Blacklisted words
+		let removeWords = ["hells", "hell", "shit", "shits"];
 		filter.removeWords(...removeWords);
+		filter.addWords("@everyone", "@here");
+		const everyonePing = botMessage.indexOf("@everyone");
+		const herePing = botMessage.indexOf("@here");
 
-		botMessage = args.join(" ");
-
+		// Deleting the message
 		message.delete().catch();
 
+		// Checking the message for a bad word then sending the message
 		if (filter.isProfane(botMessage)) {
 			message.channel.send("I can't say that!");
+		} else if (everyonePing > -1 || herePing > -1) {
+			message.channel.send(
+				"don't try and ping everyone in the server i'm not stupid"
+			);
 		} else {
 			message.channel.send(botMessage);
 		}
